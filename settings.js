840a836e6bee07e6929bd9814f9cc552c25bb261
danidu176 <danidun606@gmail.com>
2024-11-06 08:47:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'My Sticker Pack',
-  author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'Danidu sticker pack',
+  author: 'Danidu Nirmal',
+  botName: "Kinght Bot",
+  botOwner: 'Danidu Nirmal', // Your Name
+  ownerNumber: '94715322008', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
